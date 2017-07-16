80c8e441adcbf501d309a44479dd17cff81aac23
Patheman <patykujaczynski@gmail.com>
2017-07-16 15:31:20 +0200

    Dodano URLs, Widoki (views) oraz Szablony html dla modelu Route

---
+++ szybowce/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.route_list, name='route_list'),
+    url(r'^route/(?P<pk>[0-9]+)/$', views.route_detail, name='route_detail'),
+    url(r'^route/new/$', views.route_new, name='route_new'),
+    url(r'^route/(?P<pk>[0-9]+)/edit/$', views.route_edit, name='route_edit'),
+]
--- szybowce/views.py
+++ szybowce/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Route
+from .forms import RouteForm
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def route_list(request):
+
+    routes = Route.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'szybowce/route_list.html', {'routes': routes})
+
+
+def route_detail(request, pk):
+
+    route = get_object_or_404(Route, pk=pk)
+    return render(request, 'szybowce/route_detail.html', {'route': route})
+
+
+def route_new(request):
+
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        if form.is_valid():
+            route = form.save(commit=False)
+            route.author = request.user
+            route.published_date = timezone.now()
+            route.save()
+            return redirect('route_detail', pk=route.pk)
+    else:
+        form = RouteForm()
+    return render(request, 'szybowce/route_edit.html', {'form': form})
+
+
+def route_edit(request, pk):
+
+    route = get_object_or_404(Route, pk=pk)
+    if request.method == "POST":
+        form = RouteForm(request.POST, instance=route)
+        if form.is_valid():
+            route = form.save(commit=False)
+            route.author = request.user
+            route.published_date = timezone.now()
+            route.save()
+            return redirect('route_detail', pk=route.pk)
+    else:
+        form = RouteForm(instance=route)
+    return render(request, 'szybowce/route_edit.html', {'form': form})
