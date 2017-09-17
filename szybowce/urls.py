0aecd64dcd93b6e0640ab597705eb2595295eb91
Patheman <patykujaczynski@gmail.com>
2017-09-17 16:30:02 +0200

    Modified route_edit template for better user-interface, improved pdf-file, modified route_detail template (better appearance) - the same with login panel.

--- szybowce/urls.py
+++ szybowce/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.route_list, name='route_list'),
     url(r'^route/(?P<pk>[0-9]+)/$', views.route_detail, name='route_detail'),
     url(r'^route/new/$', views.route_new, name='route_new'),
-    url(r'^route/(?P<pk>[0-9]+)/edit/$', views.route_edit, name='route_edit'),
+    url(r'^route/(?P<pk>\d+)/edit/$', views.route_edit, name='route_edit'),
+    url(r'^route/(?P<pk>\d+)/remove/$', views.route_remove, name='route_remove'),
     url(r'^route/(?P<pk>[0-9]+)/pdf/$', views.pdf_gen, name='pdf_gen'),
 ]
--- szybowce/views.py
+++ szybowce/views.py
@@ -19,7 +19,7 @@ pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf'))
 pdfmetrics.registerFont(TTFont('VeraBd', 'VeraBd.ttf'))
 pdfmetrics.registerFont(TTFont('VeraIt', 'VeraIt.ttf'))
 pdfmetrics.registerFont(TTFont('VeraBI', 'VeraBI.ttf'))
-
+pdfmetrics.registerFont(TTFont('Pacifico', 'Pacifico-Regular.ttf'))
 
 # Create your views here.
 
@@ -35,69 +35,108 @@ def pdf_gen(request, pk):
      response['Content-Disposition'] = 'attachment; filename="twoja_trasa.pdf"'
      # Create the PDF object, using the response object as its "file."
      p = canvas.Canvas(response)
+     p.setFont("Pacifico", 32)
+     p.drawString(400, 800, "ppszybowce")
+
      p.setFont("Verdana", 12)
      # Get the Route object
      route = get_object_or_404(Route, pk=pk)
 
      # Draw things on the PDF. Here's where the PDF generation happens.
      # See the ReportLab documentation for the full list of functionality.
-     
-     p.drawString(25, 800, "TWOJA TRASA:")
-     x=2
+     x=2 
+     p.drawString(25, 800-25*x, "TWOJA TRASA:")
+     x=3
      if route.position1:
         p.drawString(25, 800-25*x, "Pozycja 1:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position1))
-        p.drawString(25, 800-25*(x+1), "Kąt 1:")
-        p.drawString(100, 800-25*(x+1), route.heading1)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading1)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading1), route.wind_speed), route.plane_speed), route.position1), route.position2)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position1, route.position2)))
+        x=x+3
      if route.position2:
         p.drawString(25, 800-25*x, "Pozycja 2:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position2))
-        p.drawString(25, 800-25*(x+1), "Kąt 2:")
-        p.drawString(100, 800-25*(x+1), route.heading2)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading2)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading2), route.wind_speed), route.plane_speed), route.position2), route.position3)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position2, route.position3)))
+        x=x+3
      if route.position3:
         p.drawString(25, 800-25*x, "Pozycja 3:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position3))
-        p.drawString(25, 800-25*(x+1), "Kąt 3:")
-        p.drawString(100, 800-25*(x+1), route.heading3)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading3)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading3), route.wind_speed), route.plane_speed), route.position3), route.position4)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position3, route.position4)))
+        x=x+3
      if route.position4:
         p.drawString(25, 800-25*x, "Pozycja 4:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position4))
-        p.drawString(25, 800-25*(x+1), "Kąt 4:")
-        p.drawString(100, 800-25*(x+1), route.heading4)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading4)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading4), route.wind_speed), route.plane_speed), route.position4), route.position5)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position4, route.position5)))
+        x=x+3
      if route.position5:
         p.drawString(25, 800-25*x, "Pozycja 5:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position5))
-        p.drawString(25, 800-25*(x+1), "Kąt 5:")
-        p.drawString(100, 800-25*(x+1), route.heading5)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading5)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading5), route.wind_speed), route.plane_speed), route.position5), route.position6)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position5, route.position6)))
+        x=x+3
      if route.position6:
         p.drawString(25, 800-25*x, "Pozycja 6:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position6))
-        p.drawString(25, 800-25*(x+1), "Kąt 6:")
-        p.drawString(100, 800-25*(x+1), route.heading6)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading6)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading6), route.wind_speed), route.plane_speed), route.position6), route.position7)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position6, route.position7)))
+        x=x+3
      if route.position7:
         p.drawString(25, 800-25*x, "Pozycja 7:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position7))
-        p.drawString(25, 800-25*(x+1), "Kąt 7:")
-        p.drawString(100, 800-25*(x+1), route.heading7)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading7)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading7), route.wind_speed), route.plane_speed), route.position7), route.position8)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position7, route.position8)))
+        x=x+3
      if route.position8:
         p.drawString(25, 800-25*x, "Pozycja 8:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position8))
-        p.drawString(25, 800-25*(x+1), "Kąt 8:")
-        p.drawString(100, 800-25*(x+1), route.heading8)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading8)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading8), route.wind_speed), route.plane_speed), route.position8), route.position9)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position8, route.position9)))
+        x=x+3
      if route.position9:
         p.drawString(25, 800-25*x, "Pozycja 9:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position9))
-        p.drawString(25, 800-25*(x+1), "Kąt 9:")
-        p.drawString(100, 800-25*(x+1), route.heading4)
-        x=x+2
+        p.drawString(25, 800-25*(x+1), "Kąt [*]:")
+        p.drawString(90, 800-25*(x+1), str(first.angle_m(first.angle_z(first.angle_z2(route.wind_angle, route.plane_speed), route.wind_speed), route.heading9)))
+        p.drawString(150, 800-25*(x+1), "Czas [h]:")
+        p.drawString(225, 800-25*(x+1), str(first.time2(first.time1(first.ground_speed(first.ground_speed2(first.angle_add(route.wind_angle, route.heading9), route.wind_speed), route.plane_speed), route.position9), route.position10)))
+        p.drawString(275, 800-25*(x+1), "Odległość [km]:")
+        p.drawString(390, 800-25*(x+1), str(first.calc_dist(route.position9, route.position10)))
+        x=x+3
      if route.position10:
         p.drawString(25, 800-25*x, "Pozycja 10:") 
         p.drawString(100, 800-25*x, first.gps_name(route.position10))
@@ -145,3 +184,10 @@ def route_edit(request, pk):
     else:
         form = RouteForm(instance=route)
     return render(request, 'szybowce/route_edit.html', {'form': form})
+
+
+@login_required
+def route_remove(request, pk):
+    route = get_object_or_404(Route, pk=pk)
+    route.delete()
+    return redirect('route_list')
